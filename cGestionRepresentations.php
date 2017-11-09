2bbc809b1a13d0a2628a11b515f155c3e11c1ac8
cbernard <cbernard@btssio-VirtualBox>
2017-11-09 17:35:09 +0100

    J'ai modifier le contrôleur "cGestionsRepresentations.php" en se basant sur les autres contrôleurs crées préalablement, cette modification à pour but de contrôler la rubrique "Représentation"
    Clément BERNARD

--- cGestionRepresentations.php
+++ cGestionRepresentations.php
@@ -28,103 +28,12 @@ switch ($action) {
         include("vues/Representations/vConsulterRepresentations.php");
         break;
 
-    case 'detailRepresentations':
-        $id = $_REQUEST['id'];
-        include("vues/Representations/vConsulterDetailRepresentations.php");
-        break;
-
-    case 'demanderSupprimerRepresentations':
-        $id = $_REQUEST['id'];
-        include("vues/Representations/vSupprimerRepresentations.php");
-        break;
-
-    case 'demanderCreerRepresentations':
-        include("vues/Representations/vCreerModifierRepresentations.php");
-        break;
-
-    case 'demanderModifierRepresentations':
-        $id = $_REQUEST['id'];
-        include("vues/Representations/vCreerModifierRepresentations.php");
-        break;
-
-    case 'validerSupprimerRepresentations':
-        $id = $_REQUEST['id'];
-        RepresentationDAO::delete($id);
-        include("vues/Representations/vConsulterRepresentations.php");
-        break;
-    
-
-    case 'validerCreerRepresentations':case 'validerModifierRepresentations':
-        $id = $_REQUEST['id'];
-        $idLieu = $_REQUEST['idLieu'];
-        $idGroupe = $_REQUEST['idGroupe'];
-        $DateRep = $_REQUEST['DateRep'];
-        $HeureDebut = $_REQUEST['HeureDebut'];
-        $HeureFin = $_REQUEST['HeureFin'];
- 
-        
-        if ($action == 'validerCreerRepresentations') {
-            verifierDonneesRepresentationsC ($id, $idLieu ,$idGroupe, $DateRep, $HeureDebut, $HeureFin);         
-            if (nbErreurs() == 0) {
-                $unRepresentation = new Representation($id, $idLieu, $idGroupe, $DateRep, $HeureDebut,$HeureFin); 
-                RepresentationDAO::insert($uneRepresentation);
-                include("vues/Representations/vConsulterRepresentations.php");
-            } else {
-                include("vues/Representations/vCreerModifierRepresentations.php");
-            }
-        } else {
-            verifierDonneesRepresentationsM ($id, $idLieu, $idGroupe, $DateRep, $HeureDebut, $HeureFin); 
-            if (nbErreurs() == 0) {
-                $uneRepresentation = new Representation($id, $idLieu, $idGroupe, $DateRep, $HeureDebut, $HeureFin);
-                RepresentationDAO::update($id, $uneRepresentation);
-                include("vues/Representations/vConsulterRepresentations.php");
-            } else {
-                include("vues/Representations/vCreerModifierRepresentations.php");
-            }
-        }
-        break;
+   
 }
 
 // Fermeture de la connexion au serveur MySql
 Bdd::deconnecter();
 
-function verifierDonneesRepresentationC($id, $idLieu, $idGroupe, $DateRep, $HeureDebut, $HeureFin) {
-    if ($id == "" || $idLieu == "" || $idGroupe == "" || $DateRep == "" || $HeureDebut == "" ||
-            $HeureFin == "" ) {
-        ajouterErreur('Chaque champ suivi du caractère * est obligatoire');
-    }
-    if ($id != "") {
-        // Si l'id est constitué d'autres caractères que de lettres non accentuées 
-        // et de chiffres, une erreur est générée
-        if (!estChiffresOuEtLettres($id)) {
-            ajouterErreur
-                    ("L'identifiant doit comporter uniquement des lettres non accentuées et des chiffres");
-        } else {
-            if (RepresentationDAO::isAnExistingId($id)) {
-                ajouterErreur("La Representation $id existe déjà");
-            }
-        }
-        if (!estChiffres($idLieu)){
-            ajouterErreur("L'id Lieu de la Representation doit comporter uniquement des chiffres !");
-        }
-        
-        if (!estChiffresOuEtLettres($idGroupe)){
-            ajouterErreur("L'id Groupe de la Representation doit comporter uniquement des chiffres et des lettres !");
-        }
-    }
-      
-}
-
-
-function verifierDonneesRepresentationM($id, $idLieu, $idGroupe, $DateRep, $HeureDebut, $HeureFin) {
-    if ($idLieu == "" || $identite == "" || $adresse == "" || $ville == "" ||
-            $tel == "" || $nomResponsable == "" || $adresseElectronique== "") {
-        ajouterErreur('Chaque champ suivi du caractère * est obligatoire');
-    }
-    
-
-
-}
 
    
 
